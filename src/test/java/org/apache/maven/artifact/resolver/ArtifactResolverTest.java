6db1b9d687dc1e0cb1ff87e3c8f53320b0b1240b
John Dennis Casey <jdcasey@apache.org>
2007-11-15 04:57:44 +0000

    Adding an aspect for caching resolution results. It's not enabled yet, since it has bugs and needs a little work.
    git-svn-id: https://svn.apache.org/repos/asf/maven/artifact/trunk@595206 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/artifact/resolver/ArtifactResolverTest.java
+++ src/test/java/org/apache/maven/artifact/resolver/ArtifactResolverTest.java
@@ -51,6 +51,7 @@ public class ArtifactResolverTest
 
     private Artifact projectArtifact;
 
+    @Override
     protected void setUp()
         throws Exception
     {
@@ -61,6 +62,7 @@ public class ArtifactResolverTest
         projectArtifact = createLocalArtifact( "project", "3.0" );
     }
 
+    @Override
     protected String component()
     {
         return "resolver";
@@ -87,6 +89,7 @@ public class ArtifactResolverTest
         assertLocalArtifactPresent( b );
     }
 
+    @Override
     protected Artifact createArtifact( String groupId, String artifactId, String version, String type )
         throws Exception
     {
